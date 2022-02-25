28b6f1b21b8f7bad370dfc07f25c4a22019cb50f
Jamie <83338941+JamiroFerrara@users.noreply.github.com>
2022-02-25 16:36:53 +0100

    Update

--- cut.ps1
+++ cut.ps1
@@ -1,6 +1,6 @@
 if ($args.count -eq 2)
 {
-    $destination = es -sort date-accessed /ad $args[1]| ? {$_ -notmatch "Windows"} | ? {$_ -notmatch "Microsoft"} | fzf --height 20% --reverse
+    $destination = es -sort date-accessed /ad $args[1]| ? {$_ -notmatch "Windows"} | ? {$_ -notmatch "Microsoft"} | fzf -f $args[0] |fzf --height 20% --reverse
     cp -recurse $args[0] $destination
     rm -r -fo $args[0]
 
